4081193bd7353cd12fff8ffc43df57704b00149e
Sam Tanner <samdtanner1989@gmail.com>
2023-11-26 18:22:54 +0000

    added optional challenge

---
+++ T09/optional_challenge.py
@@ -0,0 +1,16 @@
+# This is a python program to demonstrate 2 compilation errors a runtime error and logical error.
+
+print(Welcome to my test program) #Syntax: No quotations added
+
+# Shopping List
+list = ["Items:","Bread", "Milk", "Coffee"] # Logical: Added str 'Items:' to list
+
+# Find the amount of items in list
+total_items = len(list) # This will include 'Items:'
+
+# For loops to print each item in list
+for item in list:
+    print(items) # Syntax: Incorrect name. # Runtime cannot print list item
+    
+# Print amount of items in list
+print(total_items)
