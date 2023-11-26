b45633bc4cc3f787d83d2c3cab86cd4642ddeb13
Sam Tanner <samdtanner1989@gmail.com>
2023-11-26 11:49:36 +0000

    added errors2

---
+++ T09/errors2.py
@@ -0,0 +1,11 @@
+# This example program is meant to demonstrate errors.
+ 
+# There are some errors in this program. Run the program, look at the error messages, and find and fix the errors.
+
+animal = "Lion" # Runtime: Lion not defined.# Syntax: Added missing "",
+animal_type = "cub"
+number_of_teeth = 16
+
+full_spec = f"This is a {animal}. It is a {animal_type} and it has {number_of_teeth} teeth" # Logical: Missing formatting, animal type and # of teeth wrong way around
+
+print(full_spec) # Syntax: missing brackets, removed whitespace
