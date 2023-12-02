5244c33c1c728aaa6f284ce3a514809620b953dd
Sam Tanner <samdtanner1989@gmail.com>
2023-12-02 19:03:27 +0000

    Updates based on feedback

--- T07/while.py
+++ T07/while.py
@@ -1,16 +1,16 @@
 # A python program that asks for inputs until the user enters '-1'
-# Program will then calculate the average of the numbers entered not incuding '-1'
+# Program will then calculate the average of the numbers entered not including '-1'
 
 # Create list of future inputs
 total_input = []
-# Declare inital input converted to an integer
+# Declare initial input converted to an integer
 number = int(input("Please enter a number - Type '-1' to quit: "))
 
 # While loop asking for input and check if '-1' is entered
 while number > -1:
   # Add input to list
   total_input.append(number)
-  # Summarise and ask for another input
+  # Summerize and ask for another input
   print(f"\nCurrent list: {total_input}")
   number = int(input("Please enter a number - Type '-1' to quit: "))
 
