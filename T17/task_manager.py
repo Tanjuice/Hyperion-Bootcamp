afb9ce6a897899aa38ad3663f3f33ee508853cd4
Tanjuice <samdtanner1989@gmail.com>
2024-02-29 14:19:33 +0000

    Fixed incorrect indentation

--- T17/task_manager.py
+++ T17/task_manager.py
@@ -198,14 +198,14 @@ def add_task():
     (i.e. includes spacing and labelling) '''
 def view_all():
 
-        for t in task_list:
-            disp_str = f"Task: \t\t {t['title']}\n"
-            disp_str += f"Assigned to: \t {t['username']}\n"
-            disp_str += f"Date Assigned: \t {t['assigned_date'].strftime(DATETIME_STRING_FORMAT)}\n"
-            disp_str += f"Due Date: \t {t['due_date'].strftime(DATETIME_STRING_FORMAT)}\n"
-            disp_str += f"Task Description: \n {t['description']}\n"
-            disp_str += f"Completed?: \t {t['completed']}\n"
-            print(disp_str)
+    for t in task_list:
+        disp_str = f"Task: \t\t {t['title']}\n"
+        disp_str += f"Assigned to: \t {t['username']}\n"
+        disp_str += f"Date Assigned: \t {t['assigned_date'].strftime(DATETIME_STRING_FORMAT)}\n"
+        disp_str += f"Due Date: \t {t['due_date'].strftime(DATETIME_STRING_FORMAT)}\n"
+        disp_str += f"Task Description: \n {t['description']}\n"
+        disp_str += f"Completed?: \t {t['completed']}\n"
+        print(disp_str)
 
 #====View Mine function====
 ''' Reads the tasks from task.txt file and returns tasks assigned 
